fe85fa1840d0f2cf7e3f285b3d1bbbb8fb5c363f
Marek Belski <marek.belski@gmail.com>
2024-11-30 17:54:16 -0800

    Updates credits with end message.

--- scenes/credits/credits.gd
+++ scenes/credits/credits.gd
@@ -1,2 +1,6 @@
 @tool
 extends Credits
+
+func is_end_reached():
+	var _end_of_credits_vertical = %CreditsLabel.size.y + %HeaderSpace.size.y + %TextureRect.size.y
+	return $ScrollContainer.scroll_vertical > _end_of_credits_vertical
